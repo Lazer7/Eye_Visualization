4f4035e22c94d37b760b231d7e369c01810ef58c
ry101 <31788821+ry101@users.noreply.github.com>
2019-11-06 12:25:58 -0800

    session comparison

---
+++ Data/Others/participant_data.js
@@ -0,0 +1,498 @@
+var participants = [
+    {
+      "ID": "p1",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.608695652,
+      "Time_On_Task": 26,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p1",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 38,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p3",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 10,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p3",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 50,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p5",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 22,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p5",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 27,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p7",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 16,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p7",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.086956522,
+      "Time_On_Task": 20,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p11",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 15,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p11",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.347826087,
+      "Time_On_Task": 33,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p13",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 35,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p13",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 27,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p15",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 9,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p15",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 13,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p17",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 14,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p17",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.608695652,
+      "Time_On_Task": 22,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p19",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 14,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p19",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 72,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p21",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 16,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p21",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 6,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p23",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 13,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p23",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 58,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p25",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 34,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p25",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.347826087,
+      "Time_On_Task": 39,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p27",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.260869565,
+      "Time_On_Task": 13,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p27",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.086956522,
+      "Time_On_Task": 50,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p31",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 14,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p31",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 44,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p33",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 22,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p33",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.347826087,
+      "Time_On_Task": 27,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p35",
+      "Ontologies": 1,
+      "Visualization": 1,
+      "Task_Success": 0.608695652,
+      "Time_On_Task": 9,
+      "Session_Type": 1
+    },
+    {
+      "ID": "p35",
+      "Ontologies": 2,
+      "Visualization": 2,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 37,
+      "Session_Type": 4
+    },
+    {
+      "ID": "p2",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.347826087,
+      "Time_On_Task": 19,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p2",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 25,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p4",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 12,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p4",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 9,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p6",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.869565217,
+      "Time_On_Task": 26,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p6",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.739130435,
+      "Time_On_Task": 43,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p10",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.608695652,
+      "Time_On_Task": 17,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p10",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 29,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p12",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.304347826,
+      "Time_On_Task": 17,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p12",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.695652174,
+      "Time_On_Task": 11,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p14",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 15,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p14",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.217391304,
+      "Time_On_Task": 28,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p16",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 28,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p16",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 29,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p18",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.739130435,
+      "Time_On_Task": 21,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p18",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 27,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p20",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 30,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p20",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 23,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p24",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 32,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p24",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.739130435,
+      "Time_On_Task": 25,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p28",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 10,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p28",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 13,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p30",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.391304348,
+      "Time_On_Task": 23,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p30",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.47826087,
+      "Time_On_Task": 32,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p32",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.304347826,
+      "Time_On_Task": 21,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p32",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.347826087,
+      "Time_On_Task": 12,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p34",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.608695652,
+      "Time_On_Task": 38,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p34",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.565217391,
+      "Time_On_Task": 49,
+      "Session_Type": 2
+    },
+    {
+      "ID": "p36",
+      "Ontologies": 1,
+      "Visualization": 2,
+      "Task_Success": 0.434782609,
+      "Time_On_Task": 21,
+      "Session_Type": 3
+    },
+    {
+      "ID": "p36",
+      "Ontologies": 2,
+      "Visualization": 1,
+      "Task_Success": 0.52173913,
+      "Time_On_Task": 15,
+      "Session_Type": 2
+    }
+  ]
